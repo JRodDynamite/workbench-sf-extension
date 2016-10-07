c4097b784ca659e0d16e46cb3a12835137558e6b
Jason Estibeiro <jasonestibeiro@live.com>
2016-10-07 09:58:14 +0530

    initial commit

---
+++ app/scripts/login.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log('\'Allo \'Allo! Content');
+console.log(typeof jQuery);
+
+// var loginButton = $('#loginBtn').parent();
+
+// loginButton.insertAfter(`<button id='prodLogin' type='button'>Prod v37</button>`);
+
+// var div = document.createElement('DIV');
+// div.setAttribute('id', 'customButtons');
+// loginButton.parentElement.appendChild(div);
+
+// var btn = document.createElement('BUTTON');
+// btn.setAttribute('type', 'button');
+// btn.setAttribute('id', 'prodLogin');
+// var t = document.createTextNode('Prod v37');
+// btn.appendChild(t);
+// div.appendChild(btn);
+// // loginButton.insertBefore(btn, loginButton.firstChild);
+
+// var btn = document.createElement('BUTTON');
+// btn.setAttribute('type', 'button');
+// btn.setAttribute('id', 'devLogin');
+// var t = document.createTextNode('Dev v37');
+// btn.appendChild(t);
+// div.appendChild(btn);
+// loginButton.insertBefore(btn, loginButton.firstChild);
+
+// document.getElementById('prodLogin').addEventListener('click', function(event){ 
+// 	event.preventDefault();
+// 	alert('blah');
+// }, false);
